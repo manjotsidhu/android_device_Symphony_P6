2e1bef9dc15465f77f66ea81b163b49bb59c5516
Manjot Sidhu <manjot.gni@gmail.com>
2018-01-19 22:39:12 +0530

    Revert "Trying to fix Incoming Call [1]"
    This reverts commit c73e6128cefc8fa7f633ebe6658e2a188daf9fa8.

--- ril/telephony/java/com/android/internal/telephony/MediaTekRIL.java
+++ ril/telephony/java/com/android/internal/telephony/MediaTekRIL.java
@@ -18,184 +18,53 @@ package com.android.internal.telephony;
 
 import static com.android.internal.telephony.RILConstants.*;
 
+import com.android.internal.telephony.uicc.IccRefreshResponse;
 import android.content.Context;
-import android.net.LocalSocket;
-import android.net.LocalSocketAddress;
 import android.os.AsyncResult;
-import android.os.HandlerThread;
-import android.os.Looper;
 import android.os.Message;
 import android.os.Parcel;
-import android.os.SystemProperties;
-import android.text.TextUtils;
-import android.telephony.PhoneNumberUtils;
 import android.telephony.Rlog;
-import android.telephony.SignalStrength;
-
-import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-
-import java.io.IOException;
-import java.io.InputStream;
-
-import com.android.internal.telephony.uicc.IccUtils;
-import com.android.internal.telephony.uicc.IccRecords;
-import com.android.internal.telephony.uicc.UiccController;
-import com.android.internal.telephony.uicc.IccRefreshResponse;
 
 public class MediaTekRIL extends RIL implements CommandsInterface {
 
-    // MediaTek Custom States
-    static final int RIL_REQUEST_MTK_BASE = 2000;
-    static final int RIL_REQUEST_HANGUP_ALL = (RIL_REQUEST_MTK_BASE + 0);
-    static final int RIL_REQUEST_GET_COLP = (RIL_REQUEST_MTK_BASE + 1);
-    static final int RIL_REQUEST_SET_COLP = (RIL_REQUEST_MTK_BASE + 2);
-    static final int RIL_REQUEST_GET_COLR = (RIL_REQUEST_MTK_BASE + 3);
-    static final int RIL_REQUEST_GET_CCM = (RIL_REQUEST_MTK_BASE + 4);
-    static final int RIL_REQUEST_GET_ACM = (RIL_REQUEST_MTK_BASE + 5);
-    static final int RIL_REQUEST_GET_ACMMAX = (RIL_REQUEST_MTK_BASE + 6);
-    static final int RIL_REQUEST_GET_PPU_AND_CURRENCY = (RIL_REQUEST_MTK_BASE + 7);
-    static final int RIL_REQUEST_SET_ACMMAX = (RIL_REQUEST_MTK_BASE + 8);
-    static final int RIL_REQUEST_RESET_ACM = (RIL_REQUEST_MTK_BASE + 9);
-    static final int RIL_REQUEST_SET_PPU_AND_CURRENCY = (RIL_REQUEST_MTK_BASE + 10);
-    static final int RIL_REQUEST_RADIO_POWEROFF = (RIL_REQUEST_MTK_BASE + 11);       
-    static final int RIL_REQUEST_DUAL_SIM_MODE_SWITCH = (RIL_REQUEST_MTK_BASE + 12); 
-    static final int RIL_REQUEST_QUERY_PHB_STORAGE_INFO = (RIL_REQUEST_MTK_BASE + 13);       
-    static final int RIL_REQUEST_WRITE_PHB_ENTRY = (RIL_REQUEST_MTK_BASE + 14);      
-    static final int RIL_REQUEST_READ_PHB_ENTRY = (RIL_REQUEST_MTK_BASE + 15);       
-    static final int RIL_REQUEST_SET_GPRS_CONNECT_TYPE = (RIL_REQUEST_MTK_BASE + 16);
-    static final int RIL_REQUEST_SET_GPRS_TRANSFER_TYPE = (RIL_REQUEST_MTK_BASE + 17);
-    static final int RIL_REQUEST_MOBILEREVISION_AND_IMEI = (RIL_REQUEST_MTK_BASE + 18);//Add by mtk80372 for Barcode Number
-    static final int RIL_REQUEST_QUERY_SIM_NETWORK_LOCK = (RIL_REQUEST_MTK_BASE + 19);
-    static final int RIL_REQUEST_SET_SIM_NETWORK_LOCK = (RIL_REQUEST_MTK_BASE + 20);
-    static final int RIL_REQUEST_SET_SCRI = (RIL_REQUEST_MTK_BASE + 21);   
-    /* cage_vt start */
-    static final int RIL_REQUEST_VT_DIAL = (RIL_REQUEST_MTK_BASE + 22);
-    static final int RIL_REQUEST_VOICE_ACCEPT = (RIL_REQUEST_MTK_BASE + 32);
-    /* cage_vt end */
-    static final int RIL_REQUEST_BTSIM_CONNECT = (RIL_REQUEST_MTK_BASE + 23);
-    static final int RIL_REQUEST_BTSIM_DISCONNECT_OR_POWEROFF = (RIL_REQUEST_MTK_BASE + 24);
-    static final int RIL_REQUEST_BTSIM_POWERON_OR_RESETSIM = (RIL_REQUEST_MTK_BASE + 25);
-    static final int RIL_REQUEST_BTSIM_TRANSFERAPDU = (RIL_REQUEST_MTK_BASE + 26);
-    static final int RIL_REQUEST_EMERGENCY_DIAL = (RIL_REQUEST_MTK_BASE + 27);
-    static final int RIL_REQUEST_SET_NETWORK_SELECTION_MANUAL_WITH_ACT = (RIL_REQUEST_MTK_BASE + 28);
-    static final int RIL_REQUEST_QUERY_ICCID = (RIL_REQUEST_MTK_BASE + 29);
-    static final int RIL_REQUEST_SIM_AUTHENTICATION = (RIL_REQUEST_MTK_BASE + 30);   
-    static final int RIL_REQUEST_USIM_AUTHENTICATION = (RIL_REQUEST_MTK_BASE + 31); 
-    static final int RIL_REQUEST_RADIO_POWERON = (RIL_REQUEST_MTK_BASE + 33);
-    static final int RIL_REQUEST_GET_SMS_SIM_MEM_STATUS = (RIL_REQUEST_MTK_BASE + 34);
-    static final int RIL_REQUEST_FORCE_RELEASE_CALL = (RIL_REQUEST_MTK_BASE + 35);
-    static final int RIL_REQUEST_SET_CALL_INDICATION = (RIL_REQUEST_MTK_BASE + 36);
-    static final int RIL_REQUEST_REPLACE_VT_CALL = (RIL_REQUEST_MTK_BASE + 37);
-    /* 3G switch start */
-    static final int RIL_REQUEST_GET_3G_CAPABILITY = (RIL_REQUEST_MTK_BASE + 38);
-    static final int RIL_REQUEST_SET_3G_CAPABILITY = (RIL_REQUEST_MTK_BASE + 39);
-    /* 3G switch end */
-    /* User controlled PLMN selector with Access Technology  begin */
-    static final int RIL_REQUEST_GET_POL_CAPABILITY = (RIL_REQUEST_MTK_BASE + 40);
-    static final int RIL_REQUEST_GET_POL_LIST = (RIL_REQUEST_MTK_BASE + 41);
-    static final int RIL_REQUEST_SET_POL_ENTRY = (RIL_REQUEST_MTK_BASE + 42);
-    /* User controlled PLMN selector with Access Technology  end */
-    /* UPB start */
-    static final int RIL_REQUEST_QUERY_UPB_CAPABILITY = (RIL_REQUEST_MTK_BASE + 43);
-    static final int RIL_REQUEST_EDIT_UPB_ENTRY = (RIL_REQUEST_MTK_BASE + 44);
-    static final int RIL_REQUEST_DELETE_UPB_ENTRY = (RIL_REQUEST_MTK_BASE + 45);
-    static final int RIL_REQUEST_READ_UPB_GAS_LIST = (RIL_REQUEST_MTK_BASE + 46);
-    static final int RIL_REQUEST_READ_UPB_GRP = (RIL_REQUEST_MTK_BASE + 47);
-    static final int RIL_REQUEST_WRITE_UPB_GRP = (RIL_REQUEST_MTK_BASE + 48);
-    /* UPB end */
-    static final int RIL_REQUEST_DISABLE_VT_CAPABILITY = (RIL_REQUEST_MTK_BASE + 49);
-    static final int RIL_REQUEST_HANGUP_ALL_EX = (RIL_REQUEST_MTK_BASE + 50);
-    static final int RIL_REQUEST_SET_SIM_RECOVERY_ON = (RIL_REQUEST_MTK_BASE + 51);
-    static final int RIL_REQUEST_GET_SIM_RECOVERY_ON = (RIL_REQUEST_MTK_BASE + 52);
-    static final int RIL_REQUEST_SET_TRM = (RIL_REQUEST_MTK_BASE + 53);
-    static final int RIL_REQUEST_DETECT_SIM_MISSING = (RIL_REQUEST_MTK_BASE + 54);
-    static final int RIL_REQUEST_GET_CALIBRATION_DATA = (RIL_REQUEST_MTK_BASE + 55);
-
-     //For LGE APIs start
-    static final int RIL_REQUEST_GET_PHB_STRING_LENGTH = (RIL_REQUEST_MTK_BASE + 56);
-    static final int RIL_REQUEST_GET_PHB_MEM_STORAGE = (RIL_REQUEST_MTK_BASE + 57);
-    static final int RIL_REQUEST_SET_PHB_MEM_STORAGE = (RIL_REQUEST_MTK_BASE + 58);
-    static final int RIL_REQUEST_READ_PHB_ENTRY_EXT = (RIL_REQUEST_MTK_BASE + 59);
-    static final int RIL_REQUEST_WRITE_PHB_ENTRY_EXT = (RIL_REQUEST_MTK_BASE + 60);
-    
-    // requests for read/write EFsmsp
-    static final int RIL_REQUEST_GET_SMS_PARAMS = (RIL_REQUEST_MTK_BASE + 61);
-    static final int RIL_REQUEST_SET_SMS_PARAMS = (RIL_REQUEST_MTK_BASE + 62);
-
-    // NFC SEEK start
-    static final int RIL_REQUEST_SIM_TRANSMIT_BASIC = (RIL_REQUEST_MTK_BASE + 63);
-    static final int RIL_REQUEST_SIM_OPEN_CHANNEL = (RIL_REQUEST_MTK_BASE + 64);
-    static final int RIL_REQUEST_SIM_CLOSE_CHANNEL = (RIL_REQUEST_MTK_BASE + 65);
-    static final int RIL_REQUEST_SIM_TRANSMIT_CHANNEL = (RIL_REQUEST_MTK_BASE + 66);
-    static final int RIL_REQUEST_SIM_GET_ATR = (RIL_REQUEST_MTK_BASE + 67);
-    // NFC SEEK end
-
-    // CB extension
-    static final int RIL_REQUEST_SET_CB_CHANNEL_CONFIG_INFO = (RIL_REQUEST_MTK_BASE + 68);
-    static final int RIL_REQUEST_SET_CB_LANGUAGE_CONFIG_INFO = (RIL_REQUEST_MTK_BASE + 69);
-    static final int RIL_REQUEST_GET_CB_CONFIG_INFO = (RIL_REQUEST_MTK_BASE + 70);
-    static final int RIL_REQUEST_SET_ALL_CB_LANGUAGE_ON = (RIL_REQUEST_MTK_BASE + 71);
-    // CB extension
-    
-    static final int RIL_REQUEST_SET_ETWS = (RIL_REQUEST_MTK_BASE + 72);
-
-    // [New R8 modem FD]
-    static final int RIL_REQUEST_SET_FD_MODE = (RIL_REQUEST_MTK_BASE + 73);
-
-    static final int RIL_REQUEST_SIM_OPEN_CHANNEL_WITH_SW = (RIL_REQUEST_MTK_BASE + 74); // NFC SEEK
-
-    static final int RIL_REQUEST_SET_CLIP = (RIL_REQUEST_MTK_BASE + 75);
-
-    //MTK-START [mtk80776] WiFi Calling
-    static final int RIL_REQUEST_UICC_SELECT_APPLICATION = (RIL_REQUEST_MTK_BASE + 76);
-    static final int RIL_REQUEST_UICC_DEACTIVATE_APPLICATION = (RIL_REQUEST_MTK_BASE + 77);
-    static final int RIL_REQUEST_UICC_APPLICATION_IO = (RIL_REQUEST_MTK_BASE + 78);
-    static final int RIL_REQUEST_UICC_AKA_AUTHENTICATE = (RIL_REQUEST_MTK_BASE + 79);
-    static final int RIL_REQUEST_UICC_GBA_AUTHENTICATE_BOOTSTRAP = (RIL_REQUEST_MTK_BASE + 80);
-    static final int RIL_REQUEST_UICC_GBA_AUTHENTICATE_NAF = (RIL_REQUEST_MTK_BASE + 81);
-    //MTK-END [mtk80776] WiFi Calling
-    static final int RIL_REQUEST_STK_EVDL_CALL_BY_AP = (RIL_REQUEST_MTK_BASE + 82);
-
-    static final int RIL_UNSOL_MTK_BASE = 3000; 
-    static final int RIL_UNSOL_NEIGHBORING_CELL_INFO = (RIL_UNSOL_MTK_BASE + 0);
-    static final int RIL_UNSOL_NETWORK_INFO = (RIL_UNSOL_MTK_BASE + 1);
-    static final int RIL_UNSOL_CALL_FORWARDING = (RIL_UNSOL_MTK_BASE + 2);
-    static final int RIL_UNSOL_CRSS_NOTIFICATION = (RIL_UNSOL_MTK_BASE + 3);
-    static final int RIL_UNSOL_CALL_PROGRESS_INFO = (RIL_UNSOL_MTK_BASE + 4);
-    static final int RIL_UNSOL_PHB_READY_NOTIFICATION = (RIL_UNSOL_MTK_BASE + 5);
-    static final int RIL_UNSOL_SPEECH_INFO = (RIL_UNSOL_MTK_BASE + 6);
-    static final int RIL_UNSOL_SIM_INSERTED_STATUS = (RIL_UNSOL_MTK_BASE + 7);
-    static final int RIL_UNSOL_RADIO_TEMPORARILY_UNAVAILABLE = (RIL_UNSOL_MTK_BASE + 8);
-    static final int RIL_UNSOL_ME_SMS_STORAGE_FULL = (RIL_UNSOL_MTK_BASE + 9);
-    static final int RIL_UNSOL_SMS_READY_NOTIFICATION = (RIL_UNSOL_MTK_BASE + 10);
-    static final int RIL_UNSOL_SCRI_RESULT = (RIL_UNSOL_MTK_BASE + 11);
-    /* cage_vt start */
-    static final int RIL_UNSOL_VT_STATUS_INFO = (RIL_UNSOL_MTK_BASE + 12);
-    static final int RIL_UNSOL_VT_RING_INFO = (RIL_UNSOL_MTK_BASE + 13);
-    /* cage_vt end */
-    static final int RIL_UNSOL_INCOMING_CALL_INDICATION = (RIL_UNSOL_MTK_BASE + 14);
-    static final int RIL_UNSOL_SIM_MISSING = (RIL_UNSOL_MTK_BASE + 15);
-    static final int RIL_UNSOL_GPRS_DETACH = (RIL_UNSOL_MTK_BASE + 16);
-    //MTK-START [mtk04070][120208][ALPS00233196] ATCI for unsolicited response
-    static final int RIL_UNSOL_ATCI_RESPONSE = (RIL_UNSOL_MTK_BASE + 17);
-    //MTK-END [mtk04070][120208][ALPS00233196] ATCI for unsolicited response
-    static final int RIL_UNSOL_SIM_RECOVERY= (RIL_UNSOL_MTK_BASE + 18);
-    static final int RIL_UNSOL_VIRTUAL_SIM_ON = (RIL_UNSOL_MTK_BASE + 19);
-    static final int RIL_UNSOL_VIRTUAL_SIM_OFF = (RIL_UNSOL_MTK_BASE + 20);
-    static final int RIL_UNSOL_INVALID_SIM = (RIL_UNSOL_MTK_BASE + 21); 
-    static final int RIL_UNSOL_RESPONSE_PS_NETWORK_STATE_CHANGED = (RIL_UNSOL_MTK_BASE + 22);
-    static final int RIL_UNSOL_RESPONSE_ACMT = (RIL_UNSOL_MTK_BASE + 23);
-    static final int RIL_UNSOL_EF_CSP_PLMN_MODE_BIT = (RIL_UNSOL_MTK_BASE + 24);
-    static final int RIL_UNSOL_IMEI_LOCK = (RIL_UNSOL_MTK_BASE + 25);
-    static final int RIL_UNSOL_RESPONSE_MMRR_STATUS_CHANGED = (RIL_UNSOL_MTK_BASE + 26);
-    static final int RIL_UNSOL_SIM_PLUG_OUT = (RIL_UNSOL_MTK_BASE + 27);
-    static final int RIL_UNSOL_SIM_PLUG_IN = (RIL_UNSOL_MTK_BASE + 28);
-    static final int RIL_UNSOL_RESPONSE_ETWS_NOTIFICATION = (RIL_UNSOL_MTK_BASE + 29);
-    static final int RIL_UNSOL_CNAP = (RIL_UNSOL_MTK_BASE + 30);
-    static final int RIL_UNSOL_STK_EVDL_CALL = (RIL_UNSOL_MTK_BASE + 31);
+    static final int RIL_REQUEST_DUAL_SIM_MODE_SWITCH = 2012;
+    static final int RIL_REQUEST_SET_GPRS_CONNECT_TYPE = 2016;
+    static final int RIL_REQUEST_SET_GPRS_TRANSFER_TYPE = 2017;
+    static final int RIL_REQUEST_SET_CALL_INDICATION = 2036;
+    static final int RIL_UNSOL_NEIGHBORING_CELL_INFO = 3000;
+    static final int RIL_UNSOL_NETWORK_INFO = 3001;
+    static final int RIL_UNSOL_CALL_FORWARDING = 3002;
+    static final int RIL_UNSOL_CRSS_NOTIFICATION = 3003;
+    static final int RIL_UNSOL_CALL_PROGRESS_INFO = 3004;
+    static final int RIL_UNSOL_PHB_READY_NOTIFICATION = 3005;
+    static final int RIL_UNSOL_SPEECH_INFO = 3006;
+    static final int RIL_UNSOL_SIM_INSERTED_STATUS = 3007;
+    static final int RIL_UNSOL_RADIO_TEMPORARILY_UNAVAILABLE = 3008;
+    static final int RIL_UNSOL_ME_SMS_STORAGE_FULL = 3009;
+    static final int RIL_UNSOL_SMS_READY_NOTIFICATION = 3010;
+    static final int RIL_UNSOL_SCRI_RESULT = 3011;
+    static final int RIL_UNSOL_VT_STATUS_INFO = 3012;
+    static final int RIL_UNSOL_VT_RING_INFO = 3013;
+    static final int RIL_UNSOL_INCOMING_CALL_INDICATION = 3014;
+    static final int RIL_UNSOL_SIM_MISSING = 3015;
+    static final int RIL_UNSOL_GPRS_DETACH = 3016;
+    static final int RIL_UNSOL_SIM_RECOVERY= 3018;
+    static final int RIL_UNSOL_VIRTUAL_SIM_ON = 3019;
+    static final int RIL_UNSOL_VIRTUAL_SIM_OFF = 3020;
+    static final int RIL_UNSOL_INVALID_SIM = 3021;
+    static final int RIL_UNSOL_RESPONSE_PS_NETWORK_STATE_CHANGED = 3022;
+    static final int RIL_UNSOL_RESPONSE_ACMT = 3023;
+    static final int RIL_UNSOL_EF_CSP_PLMN_MODE_BIT = 3024;
+    static final int RIL_UNSOL_IMEI_LOCK = 3025;
+    static final int RIL_UNSOL_RESPONSE_MMRR_STATUS_CHANGED = 3026;
+    static final int RIL_UNSOL_SIM_PLUG_OUT = 3027;
+    static final int RIL_UNSOL_SIM_PLUG_IN = 3028;
+    static final int RIL_UNSOL_RESPONSE_ETWS_NOTIFICATION = 3029;
+    static final int RIL_UNSOL_CNAP = 3030;
+    static final int RIL_UNSOL_STK_EVDL_CALL = 3031;
 
     private boolean dataAllowed = false;
+    private boolean setPreferredNetworkTypeSeen = false;
     private String voiceRegState = "0";
     private String voiceDataTech = "0";
 
@@ -487,6 +356,24 @@ public class MediaTekRIL extends RIL implements CommandsInterface {
             AsyncResult.forMessage(response, ret, null);
             response.sendToTarget();
         }
+    }
+
+    protected Object
+    responseFailCause(Parcel p) {
+        int numInts;
+        int response[];
+
+        numInts = p.readInt();
+        response = new int[numInts];
+        for (int i = 0 ; i < numInts ; i++) {
+            response[i] = p.readInt();
+        }
+        LastCallFailCause failCause = new LastCallFailCause();
+        failCause.causeCode = response[0];
+        if (p.dataAvail() > 0) {
+          failCause.vendorCause = p.readString();
+        }
+        return failCause;
     }
 
     @Override
